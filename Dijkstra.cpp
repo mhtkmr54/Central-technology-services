4222d319769231530fb7c93c3e2055763affb39d
mhtkmr54 <mhtkmr54@gmail.com>
2016-08-20 16:38:24 +0530

    dijkstra anme

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -6,12 +6,34 @@
 #include <map>
 #include <vector>
 #include <queue>
-#include <deque<>
+#include <deque>
+#include <iterator>
 
-int testcase;
-cin >> testcase;
-map
+using namespace std;
 
+int main(){
+  int testcase;
+  cin >> testcase;
+  map < int, vector<pair<int,int> > > mymapofvec;
+  for (int j=0 ; j < testcase; j++)
+  {
+    int f,s,dist;
+    cin >> f >> s >> dist;
+    vector<pair<int,int> > temp;
+    temp.push_back(make_pair(dist,s));
+    mymapofvec[f].push_back(temp);
+    delete temp;
+  }
 
-int main()
+  for (auto elem : mymapofvec)
+  {
+    cout << elem.first << " " endl;
+    for (auto el : elem.second)
+    {
+        cout << el.first << " " << el.second << " " << endl;
+    }
+  }
+
+  return 0;
+}
 
