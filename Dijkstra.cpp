b32e78c6b7fcae4b34278451f6991d4fef222fb8
mhtkmr54 <mhtkmr54@gmail.com>
2016-08-24 20:33:46 +0530

    gene

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -10,39 +10,64 @@
 #include <iterator>
 #include <string>
 
+
 using namespace std;
 
 string Dijkstra(vector<vector<int> > arr, int start, int end){
  string path;
  int pathlen = 0;
  map <int,int> vis;
+ for (int k = 0; k < end+1; k++)
+ {
+     vis.insert(pair<int,int>(k,0));
+ }
+
  int len  = arr[0].size();
+ cout << len << " arr[0].size() ";
  deque<pair<int,int> >mydeque;
  mydeque.push_back(make_pair(pathlen,start));
  int min_compare = 9999999;
  while(!mydeque.empty())
  {
+
     int path = mydeque.front().first;
     int curr  = mydeque.front().second;
-    cout << mydeque.front().first << mydeque.front().second << endl;
+    cout << " node b4 popping " << mydeque.front().second << endl;
+    mydeque.pop_front();
+    cout << "path covered till " << mydeque.front().first << " current node  " << curr << endl;
     if (vis[curr] == 0)
     {
-      vis.insert(make_pair(curr,pathlen));
+      cout << " node which popped eligible -------------------------------------------------  -----------"    << curr << endl;
+      vis[curr] = 1;
+      cout << "MAP" << endl;
+      for(auto elem : vis)
+      {
+         std::cout << elem.first << " " << elem.second << " " << "\n";
+      }
+      if (curr == end){
+        string ans  = to_string(path);
+        return ans;
+      }
       for (int k = 0; k < len; k++)
       {
-        if (arr[curr][k] != -99 )
+        if (arr[curr][k] != -99 && vis[k] == 0)
+
         {
+            cout << "visk" << vis[k] << endl;
             int edgelen = arr[curr][k];
-            if (vis[k] == 0){
+
+              cout << "adding node " << k << endl;
               mydeque.push_back(make_pair(edgelen+path ,k));
-            }
+
         }
       }
       sort(mydeque.begin(),mydeque.end());
+      cout << "mydeque" << endl;
+      for(auto elem : mydeque)
+      {
+         std::cout << elem.first << " " << elem.second << " " << "\n";
+      }
     }
-
-
-
  }
 return "voil";
 }
@@ -88,8 +113,8 @@ int main(){
      cout << endl;
   }
   cout << endl;
-  Dijkstra (arr,0,nodes-1);
-
+  string s = Dijkstra (arr,0,nodes-1);
+  cout << "ANS " << s ;
 
   return 0;
 }
