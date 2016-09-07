8309715b40d4936f969fe5cb2cd274fa5f9629f7
mhtkmr54 <mhtkmr54@gmail.com>
2016-09-07 18:12:05 +0000

    no er

--- conn_cell.cpp
+++ conn_cell.cpp
@@ -18,21 +18,21 @@ int columns;
 map <string,int> visited;
 map <string,string> parent;
 int ans_max = -999999;
-int counter = 1;
+int counter;
 
 
 
 
 void DFS_visit(int i, int j){
-	deque< deque<int, int> > dirs;
-	dirs.push_back(make_pair<int,int>(i-1,j-1));
-    dirs.push_back(make_pair<int,int>(i-1,j));
-    dirs.push_back(make_pair<int,int>(i-1,j+1));
-    dirs.push_back(make_pair<int,int>(i,j-1));
-    dirs.push_back(make_pair<int,int>(i,j+1));
-    dirs.push_back(make_pair<int,int>(i+1,j-1));
-    dirs.push_back(make_pair<int,int>(i+1,j));
-    dirs.push_back(make_pair<int,int>(i+1,j+1));
+	deque< pair<int, int> > dirs;
+	dirs.push_back(pair<int,int>(i-1,j-1));
+    dirs.push_back(pair<int,int>(i-1,j));
+    dirs.push_back(pair<int,int>(i-1,j+1));
+    dirs.push_back(pair<int,int>(i,j-1));
+    dirs.push_back(pair<int,int>(i,j+1));
+    dirs.push_back(pair<int,int>(i+1,j-1));
+    dirs.push_back(pair<int,int>(i+1,j));
+    dirs.push_back(pair<int,int>(i+1,j+1));
     string resp = to_string(i) + to_string(j);
 
 
@@ -43,7 +43,7 @@ void DFS_visit(int i, int j){
 	    	string resc = to_string(l) + to_string(m);
 	    	if (arr[l][m] != 0){ 
 	    		if (parent[resc] == ""){
-	    		visited[res] = 1;
+	    		visited[resc] = 1;
 	    		parent[resc] = resp;
 	    		counter++;
 	    	    DFS_visit(l,m);
@@ -60,20 +60,15 @@ void DFS_visit(int i, int j){
 
 
 
-
-
-
-
-
-
 void findreg(){
   for (int i =0; i < rows ; i++)
    {
     for (int j =0; j < columns; j++)
      {
-        string res = to_string(i) + to_string(j);
-        if (visited[res] == -99){
-        	visited[res] = 1;
+        string rest = to_string(i) + to_string(j);
+        if (visited[rest] == -99){
+        	visited[rest] = 1;
+        	counter = 1;
         	DFS_visit(i,j);
         	if (counter >  ans_max){
         		ans_max = counter;
@@ -120,7 +115,7 @@ int main(){
      cout << endl;
    }
    cout << endl;
-   parent['00'] = '00';
+   parent["00"] = "00";
    findreg();
    cout << ans_max << endl;
 	return 0;
