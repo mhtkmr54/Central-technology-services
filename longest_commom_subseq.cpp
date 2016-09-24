2ca427dc3d9bfef916cfed772a32ed328367066f
mhtkmr54 <mhtkmr54@gmail.com>
2016-09-24 18:38:26 +0000

    kadane

--- longest_commom_subseq.cpp
+++ longest_commom_subseq.cpp
@@ -2,27 +2,40 @@
 #include<bits/stdc++.h>
 #include <cmath>
 #include <cstdio>
-#include <vector>
+#include <deque>
 #include <iostream>
 #include <algorithm>
 #include <map>
-#include <vector>
 #include <queue>
-#include <deque>
+#include <vector>
 #include <iterator>
 #include <string>
+
 using namespace std;
+
 int max(int a, int b);
- 
+
+map <string,int> memo;
+ // http://ideone.com/uTX7nD
 /* Returns length of LCS for X[0..m-1], Y[0..n-1] */
 int lcs( char *X, char *Y, int m, int n )
 {
-   if (m == 0 || n == 0)
-     return 0;
-   if (X[m-1] == Y[n-1])
-     return 1 + lcs(X, Y, m-1, n-1);
-   else
-     return max(lcs(X, Y, m, n-1), lcs(X, Y, m-1, n));
+  string res = to_string(m) + to_string(n);
+   if (memo.find(res) != memo.end()){
+      return memo[res];
+   }
+   else{
+          if (m == 0 || n == 0){
+            return 0;
+         }  
+         if (X[m-1] == Y[n-1]){
+           return memo[res] = 1 + lcs(X, Y, m-1, n-1);
+         }
+         else{
+           return memo[res] = max(lcs(X, Y, m, n-1), lcs(X, Y, m-1, n));
+         }
+   }
+   
 }
  
 /* Utility function to get max of 2 integers */
@@ -36,13 +49,14 @@ int main()
 {
   char X[] = "AGGTAB";
   char Y[] = "GXTXAYB";
-  char in[4] ;
-  cin >> in;
-  cout << sizeof(in)/sizeof(char);
   int m = strlen(X);
   int n = strlen(Y);
  
   printf("Length of LCS is %d\n", lcs( X, Y, m, n ) );
+
+  for(auto el : memo ){
+    cout << el.first << " : " << el.second << endl;
+  }
  
   return 0;
 }
