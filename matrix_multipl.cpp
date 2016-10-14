31ac8d74e727e007df34a1ef7599cb16b0851443
mhtkmr54 <mhtkmr54@gmail.com>
2016-10-14 21:30:54 +0000

    MCQ

--- matrix_multipl.cpp
+++ matrix_multipl.cpp
@@ -1,46 +1,54 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-#include <map>
-#include <vector>
-#include <queue>
-#include <deque>
-#include <iterator>
-#include <string>
-
-
-using namespace std;
-
-
-
-for(i=0; i<r1; ++i)
-    for(j=0; j<c2; ++j)
-    for(k=0; k<c1; ++k)
+// See the Cormen book for details of the following algorithm
+#include <stdio.h>
+#include <limits.h>
+ 
+// Matrix Ai has dimension p[i-1] x p[i] for i = 1..n
+int MatrixChainOrder(int p[], int n)
+{
+ 
+    /* For simplicity of the program, one extra row and one
+       extra column are allocated in m[][].  0th row and 0th
+       column of m[][] are not used */
+    int m[n][n];
+ 
+    int i, j, k, L, q;
+ 
+    /* m[i,j] = Minimum number of scalar multiplications needed
+       to compute the matrix A[i]A[i+1]...A[j] = A[i..j] where
+       dimension of A[i] is p[i-1] x p[i] */
+ 
+    // cost is zero when multiplying one matrix.
+    for (i=1; i<n; i++)
+        m[i][i] = 0;
+ 
+    // L is chain length.
+    for (L=2; L<n; L++)
     {
-        mult[i][j]+=a[i][k]*b[k][j];
+        for (i=1; i<n-L+1; i++)
+        {
+            j = i+L-1;
+            m[i][j] = INT_MAX;
+            for (k=i; k<=j-1; k++)
+            {
+                // q = cost/scalar multiplications
+                q = m[i][k] + m[k+1][j] + p[i-1]*p[k]*p[j];
+                if (q < m[i][j])
+                    m[i][j] = q;
+            }
+        }
     }
-
-void foo(int n, int sum)
-{
-  int k = 0, j = 0;
-  if (n == 0) return;
-    k = n % 10; 
-  j = n / 10;
-  sum = sum + k;
-  foo (j, sum);
-  printf ("%d,", k);
+ 
+    return m[1][n-1];
 }
-  
-int main ()
+ 
+int main()
 {
-  int a = 2048, sum = 0;
-  char b = 'a';
-  if (a,b){
-   printf ("%d\n", a);  
-  }
-    
-  getchar();
+    int arr[] = {1, 2, 3, 4};
+    int size = sizeof(arr)/sizeof(arr[0]);
+ 
+    printf("Minimum number of multiplications is %d ",
+                       MatrixChainOrder(arr, size));
+ 
+    getchar();
+    return 0;
 }
-
