1933b9860321bc1c4811b42cbdeafdfcf6d1316a
mhtkmr54 <mhtkmr54@gmail.com>
2016-10-17 19:00:46 +0000

    missing diff

--- binary_search.cpp
+++ binary_search.cpp
@@ -24,6 +24,33 @@ int binarySearch(int arr[], int l, int r, int x)
    return -1;
 }
  
+ int findMissingUtil(int arr[], int low, int high, int diff)
+{
+    // There must be two elements to find the missing
+    if (high <= low)
+        return INT_MAX;
+ 
+    // Find index of middle element
+    int mid = low + (high - low)/2;
+ 
+    // The element just after the middle element is missing.
+    // The arr[mid+1] must exist, because we return when
+    // (low == high) and take floor of (high-low)/2
+    if (arr[mid+1] - arr[mid] != diff)
+        return (arr[mid] + diff);
+ 
+    // The element just before mid is missing
+    if (mid > 0 && arr[mid] - arr[mid-1] != diff)
+        return (arr[mid-1] + diff);
+ 
+    // If the elements till mid follow AP, then recur
+    // for right half
+    if (arr[mid] == arr[0] + mid*diff)
+        return findMissingUtil(arr, mid+1, high, diff);
+ 
+    // Else recur for left half
+    return findMissingUtil(arr, low, mid-1, diff);
+}
 int main(void)
 {
    int arr[] = {2, 3, 4, 10, 40};
